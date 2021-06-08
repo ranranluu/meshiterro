1a41e44fdc9ac3d248daf195f42d95ec093a4e1a
ranranluu <ranranluutennis@gmail.com>
2021-06-08 03:42:07 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8086b3cb141fa42ac1d7a3d7f11553c881b4b6dd282d93ac2aa3e4a1f506e7255443b6c824e197421f26d04ec1c5d0fb46bfbdfc82c4c7eca3e7dac97bfc6872'
